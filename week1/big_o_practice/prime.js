3ad642b9f6dd9370a251bff4f2312d00b73c43c3
William Hendershot <whendershot1@gmail.com>
2022-04-04 16:48:19 +0900

    Initial implementation

--- week1/big_o_practice/prime.js
+++ week1/big_o_practice/prime.js
@@ -8,7 +8,7 @@ Number.prototype.isPrime = function() {
     }
     
 const { performance } = require('perf_hooks');
-const start = performance.now();
+let start = performance.now();
 let primeCount = 0;
 let num = 2; // for math reasons, 1 is considered prime
 while( primeCount < 1e4 ) {
@@ -18,4 +18,28 @@ while( primeCount < 1e4 ) {
     num++;
 }
 console.log(`The 10,000th prime number is ${num-1}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+start = performance.now();
+primeCount = 0;
+num = 2;
+while( primeCount < 1e5 ) {
+    if( num.isPrime() ) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 100,000th prime number is ${num-1}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+start = performance.now();
+primeCount = 0;
+num = 2;
+while( primeCount < 1e6 ) {
+    if( num.isPrime() ) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 1,000,000th prime number is ${num-1}`);
 console.log(`This took ${performance.now() - start} milliseconds to run`);
