eaa1c500f9b97a60db7115f974ebb1af731a59ce
William Hendershot <whendershot1@gmail.com>
2022-04-04 16:46:06 +0900

    Correct challenge setup

--- week1/big_o_practice/reverse.js
+++ week1/big_o_practice/reverse.js
@@ -1,12 +1,2 @@
-const { performance } = require('perf_hooks');
-const start = performance.now();
-let primeCount = 0;
-let num = 2; // for math reasons, 1 is considered prime
-while( primeCount < 1e4 ) {
-    if( num.isPrime() ) {
-        primeCount++;
-    }
-    num++;
-}
-console.log(`The 10,000th prime number is ${num-1}`);
-console.log(`This took ${performance.now() - start} milliseconds to run`);
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+const reversed1 = story.split("").reverse().join("");
