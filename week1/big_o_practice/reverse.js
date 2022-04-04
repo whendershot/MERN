fdafa3c2451affc320e114111cfce3ecf31ea717
William Hendershot <whendershot1@gmail.com>
2022-04-04 16:33:33 +0900

    Challenge setup

---
+++ week1/big_o_practice/reverse.js
@@ -0,0 +1,12 @@
+const { performance } = require('perf_hooks');
+const start = performance.now();
+let primeCount = 0;
+let num = 2; // for math reasons, 1 is considered prime
+while( primeCount < 1e4 ) {
+    if( num.isPrime() ) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 10,000th prime number is ${num-1}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
