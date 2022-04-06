a5ef51a338490fa03fdb3542a4cc9a44383c69c0
William Hendershot <whendershot1@gmail.com>
2022-04-06 16:46:38 +0900

    Correct name on card title Splat props properties

--- week1/prop_it_up/src/components/PersonCard.js
+++ week1/prop_it_up/src/components/PersonCard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const PersonCard = (props) => {
+    const { firstName, 
+            lastName,
+            age,
+            hairColor
+        } = props;
+    
     return (
         <div className="card col">
             <div className="card-body">
-                <h5 className="card-title">{props.firstName}, {props.lastName}</h5>
-                <p>Age: {props.age}</p>
-                <p>Hair Color:{props.hairColor}</p>
+                <h5 className="card-title">{lastName}, {firstName}</h5>
+                <p>Age: {age}</p>
+                <p>Hair Color:{hairColor}</p>
             </div>
         </div>
     );
