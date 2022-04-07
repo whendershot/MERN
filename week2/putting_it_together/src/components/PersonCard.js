38bb61472e9f307351d6902ac83b9d64bb9aa0dd
William Hendershot <whendershot1@gmail.com>
2022-04-07 17:06:27 +0900

    Initial implementation

--- week2/putting_it_together/src/components/PersonCard.js
+++ week2/putting_it_together/src/components/PersonCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
     const { firstName, 
@@ -6,13 +6,24 @@ const PersonCard = (props) => {
             age,
             hairColor
         } = props;
+
+    const [state, setState] = useState ({
+        myAge: age
+    });
+
+    const handleClick = () => {
+        setState({
+            myAge: state.myAge + 1
+        });
+    }
     
     return (
         <div className="card col">
             <div className="card-body">
                 <h5 className="card-title">{lastName}, {firstName}</h5>
-                <p>Age: {age}</p>
+                <p>Age: {state.myAge}</p>
                 <p>Hair Color:{hairColor}</p>
+                <button onClick={ handleClick }>Birthday Button for {firstName + ' ' + lastName}</button>
             </div>
         </div>
     );
