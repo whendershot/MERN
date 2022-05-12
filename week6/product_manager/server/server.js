cc7bdc5041230574aa59f23e5bcba28ea4df2374
William Hendershot <whendershot1@gmail.com>
2022-05-12 15:44:43 +0900

    Install CORS Add Product API CRUD paths

--- week6/product_manager/server/server.js
+++ week6/product_manager/server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const app = express();
+require('./config/mongoose.config');
+const APIProductRoutes = require('./routes/product.routes');
+const cors = require('cors');
+
 const API_SERVER_PORT = 8080;
 
+const app = express();
+app.use(cors());
 app.use(express.json()), express.urlencoded({extended: true});
+
+APIProductRoutes(app);
 
 app.listen(API_SERVER_PORT, () => console.log('API server started on port: ' + API_SERVER_PORT));
