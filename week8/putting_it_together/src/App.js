d02e6967fbb08f8c886a8ac498e15dc7a7883aeb
William Hendershot <whendershot1@gmail.com>
2022-05-26 10:06:07 +0900

    Initial implementation

--- week8/putting_it_together/src/App.js
+++ week8/putting_it_together/src/App.js
@@ -6,18 +6,18 @@ function App() {
     return (
         <div className="App container">
             <div className="row">
-                <PersonCard firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"} />
-                <PersonCard firstName={"John"} lastName={"Smith"} age={"88"} hairColor={"Brown"} />
-                <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={"50"} hairColor={"Brown"} />
-                <PersonCard firstName={"Maria"} lastName={"Smith"} age={"62"} hairColor={"Brown"} />
-                <PersonCard firstName={"William"} lastName={"Hendershot"} age={"38"} hairColor={"Brown"} />
+                <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"} />
+                <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"} />
+                <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"} />
+                <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"} />
+                <PersonCard firstName={"William"} lastName={"Hendershot"} age={38} hairColor={"Brown"} />
             </div>
             <div className="row">
-                <PersonCardClass firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"} />
-                <PersonCardClass firstName={"John"} lastName={"Smith"} age={"88"} hairColor={"Brown"} />
-                <PersonCardClass firstName={"Millard"} lastName={"Fillmore"} age={"50"} hairColor={"Brown"} />
-                <PersonCardClass firstName={"Maria"} lastName={"Smith"} age={"62"} hairColor={"Brown"} />
-                <PersonCardClass firstName={"William"} lastName={"Hendershot"} age={"38"} hairColor={"Brown"} />
+                <PersonCardClass firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"} />
+                <PersonCardClass firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"} />
+                <PersonCardClass firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"} />
+                <PersonCardClass firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"} />
+                <PersonCardClass firstName={"William"} lastName={"Hendershot"} age={38} hairColor={"Brown"} />
             </div>
         </div>
     );
--- week8/putting_it_together/src/components/PersonCard.class.js
+++ week8/putting_it_together/src/components/PersonCard.class.js
@@ -2,21 +2,29 @@ import React, {Component} from "react";
 
 class PersonCardClass extends Component {
 
-    render() {
-        const {
-            firstName,
-            lastName,
-            age,
-            hairColor
-        } = this.props;
+    constructor(props) {
+        super(props);
+        this.state = {
+            _firstName: this.props.firstName,
+            _lastName: this.props.lastName,
+            _age: this.props.age,
+            _hairColor: this.props.hairColor
+        }
+    };
 
+    incAge = () => {
+        this.setState({...this.state, _age:this.state._age + 1});
+    };
+
+    render() {
         return (
             <div className="card col">
                 <div className="card-body">
-                    <h5 className="card-title">{lastName}, {firstName}</h5>
-                    <p>Age: {age}</p>
-                    <p>Hair Color:{hairColor}</p>
+                    <h5 className="card-title">{this.state._lastName}, {this.state._firstName}</h5>
+                    <p>Age: {this.state._age}</p>
+                    <p>Hair Color:{this.state._hairColor}</p>
                 </div>
+                <button className="btn btn-primary" onClick={this.incAge}>Birthday Button for {this.state._firstName} {this.state._lastName}</button>
             </div>
         );
     };
